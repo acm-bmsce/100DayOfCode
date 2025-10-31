efd87f7651396dc507ad57c2e57479db281356ac
Aprameya A Rao <rao.aprameya1205@gmail.com>
2025-10-31 23:43:42 +0530

    Deployed with updated username and password

---
+++ update.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users (username, name) VALUES ('nidhisahare','Nidhi Sahare');
+INSERT INTO Users (username, name) VALUES ('methnlove','Mithun Sidhaarth AM');
+INSERT INTO Users (username, name) VALUES ('dishaprabhakar_556','Disha P');
+
+UPDATE Users 
+SET username="Rithanyakr26"
+WHERE id=1421;
+
+
+
